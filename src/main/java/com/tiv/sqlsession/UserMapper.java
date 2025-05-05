c20da43282114d12bfe80c90f745b0929eabaf0c
tivvvv <18612553935@163.com>
2025-05-05 19:42:35 +0800

    feat: 标识查询条件的字段.

--- src/main/java/com/tiv/sqlsession/UserMapper.java
+++ src/main/java/com/tiv/sqlsession/UserMapper.java
@@ -1,6 +1,8 @@
 package com.tiv.sqlsession;
 
+import com.tiv.sqlsession.annotation.Param;
+
 public interface UserMapper {
 
-    User selectById(int id);
+    User selectById(@Param("id") int id);
 }
